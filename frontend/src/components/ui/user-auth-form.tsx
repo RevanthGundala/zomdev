296e35d2a68d39de5c914d0319c2f6bb5084c0db
RevanthGundala <93841932+RevanthGundala@users.noreply.github.com>
2024-04-22 22:50:48 -0700

    debug

--- frontend/src/components/ui/user-auth-form.tsx
+++ frontend/src/components/ui/user-auth-form.tsx
@@ -53,6 +53,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
       redirect_uri: process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI!,
       response_type: "code",
       scope: "openid",
+      ack_oob_shutdown: "2022-10-03",
     });
     console.log("Params: ", params.toString());
     const loginURL = `https://accounts.google.com/o/oauth2/v2/auth?${params}`;
