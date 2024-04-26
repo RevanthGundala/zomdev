9e54e5a4b6412ca45ef5dc0000336be73c080086
RevanthGundala <93841932+RevanthGundala@users.noreply.github.com>
2024-04-25 21:34:37 -0700

    changed frontend

--- backend/cloud/aws/lambdas/get-profile.ts
+++ backend/cloud/aws/lambdas/get-profile.ts
@@ -1,8 +1,14 @@
 import * as aws from "@pulumi/aws";
 import { APIGatewayProxyEventV2 } from "aws-lambda";
 import { ZkWalletClient, ZkProverClient } from "@shinami/clients";
-import { getZkLoginSignature, genAddressSeed } from "@mysten/zklogin";
+import {
+  getZkLoginSignature,
+  genAddressSeed,
+  generateRandomness,
+} from "@mysten/zklogin";
 import { JwtPayload, jwtDecode } from "jwt-decode";
+import { SuiClient, getFullnodeUrl } from "@mysten/sui.js/client";
+import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
 
 type PartialZkLoginSignature = Omit<
   Parameters<typeof getZkLoginSignature>["0"]["inputs"],
@@ -11,74 +17,58 @@ type PartialZkLoginSignature = Omit<
 
 export const getProfile = new aws.lambda.CallbackFunction("getProfile", {
   callback: async (event: APIGatewayProxyEventV2) => {
-    console.log("Event: ", event);
-    // const { Cookie } = event.headers;
-    // if (!Cookie)
-    //   return {
-    //     statusCode: 400,
-    //     body: "Cookie not Found",
-    //   };
-    // const jwt = Cookie.split("=")[1];
-    const { code, state: encodedState } = event.queryStringParameters || {};
-    if (!code || !encodedState) {
+    const { Cookie } = event.headers;
+    if (!Cookie)
       return {
         statusCode: 400,
-        body: "Code and/or State not Found",
+        body: "Cookie not Found",
+      };
+    const jwt = Cookie.split("=")[1];
+
+    //const walletAccessKey = process.env.SHINAMI_WALLET_ACCESS_KEY!;
+    // TODO:
+    const walletAccessKey = "1cb2a0622c007218a6c9550f25d07fa6";
+    const zkw = new ZkWalletClient(walletAccessKey);
+    const { salt, address: zkLoginUserAddress } =
+      await zkw.getOrCreateZkLoginWallet(jwt);
+
+    const zkp = new ZkProverClient(walletAccessKey);
+
+    const client = new SuiClient({ url: getFullnodeUrl("testnet") });
+    const { epoch } = await client.getLatestSuiSystemState();
+    const maxEpoch = Number(epoch) + 2;
+    const ephemeralKey = new Ed25519Keypair();
+    const jwtRandomness = generateRandomness();
+
+    const { zkProof } = await zkp.createZkLoginProof(
+      jwt,
+      maxEpoch,
+      ephemeralKey.getPublicKey(),
+      BigInt(jwtRandomness),
+      salt
+    );
+
+    const partialZkLoginSignature = zkProof as PartialZkLoginSignature;
+
+    const decodedJwt = jwtDecode<JwtPayload>(jwt);
+    let { sub, aud } = decodedJwt;
+    if (!sub || !aud) {
+      return {
+        statusCode: 400,
+        body: "Sub and/or Aud not found in JWT",
       };
     }
 
-    // const state = JSON.parse(decodeURIComponent(encodedState));
-    // //const walletAccessKey = process.env.SHINAMI_WALLET_ACCESS_KEY!;
-    // // TODO:
-    // const walletAccessKey = "1cb2a0622c007218a6c9550f25d07fa6";
-    // const zkw = new ZkWalletClient(walletAccessKey);
-    // const { salt, address: zkLoginUserAddress } =
-    //   await zkw.getOrCreateZkLoginWallet(jwt);
+    if (typeof aud !== "string") {
+      aud = aud[0];
+    }
+    const addressSeed = genAddressSeed(salt, "sub", sub, aud).toString();
 
-    // const zkp = new ZkProverClient(walletAccessKey);
+    const inputs = {
+      ...partialZkLoginSignature,
+      addressSeed,
+    };
 
-    // const { maxEpoch, ephemeralPublicKey, jwtRandomness } = state;
-
-    // const { zkProof } = await zkp.createZkLoginProof(
-    //   jwt,
-    //   maxEpoch,
-    //   ephemeralPublicKey,
-    //   jwtRandomness,
-    //   salt
-    // );
-
-    // const partialZkLoginSignature = zkProof as PartialZkLoginSignature;
-
-    // const decodedJwt = jwtDecode<JwtPayload>(jwt);
-    // let { sub, aud } = decodedJwt;
-    // if (!sub || !aud) {
-    //   return {
-    //     statusCode: 400,
-    //     body: "Sub and/or Aud not found in JWT",
-    //   };
-    // }
-
-    // if (typeof aud !== "string") {
-    //   aud = aud[0];
-    // }
-    // const addressSeed = genAddressSeed(salt, "sub", sub, aud).toString();
-
-    // const inputs = {
-    //   ...partialZkLoginSignature,
-    //   addressSeed,
-    // };
-
-    // return {
-    //   statusCode: 200,
-    //   headers: {
-    //     "Access-Control-Allow-Origin": "https://fantura.vercel.app",
-    //     "Access-Control-Allow-Credentials": true,
-    //   },
-    //   body: JSON.stringify({
-    //     zkLoginUserAddress,
-    //     inputs,
-    //   }),
-    // };
     return {
       statusCode: 200,
       headers: {
@@ -86,7 +76,10 @@ export const getProfile = new aws.lambda.CallbackFunction("getProfile", {
         "Access-Control-Allow-Credentials": true,
       },
       body: JSON.stringify({
-        timestamp: new Date().toISOString(),
+        zkLoginUserAddress,
+        maxEpoch,
+        ephemeralKey,
+        inputs,
       }),
     };
   },
