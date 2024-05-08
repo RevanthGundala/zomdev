f16afb8547a279479f610c1f07c1a5657d8fb440
RevanthGundala <93841932+RevanthGundala@users.noreply.github.com>
2024-05-08 11:27:15 -0700

    tes

--- zomdev/app/payments/page.tsx
+++ zomdev/app/payments/page.tsx
@@ -1,35 +1,39 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { redirect, useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { getCheckoutSession } from "../actions/stripe/checkout-session";
 
 export default function Payments() {
-  const sessionId = useSearchParams().get("session_id");
+  // const sessionId = useSearchParams().get("session_id");
   const [status, setStatus] = useState(null);
   const [customerEmail, setCustomerEmail] = useState("");
+  const router = useRouter();
+  // useEffect(() => {
+  //   getCheckoutSession(sessionId).then((data) => {
+  //     setStatus(data?.status as any);
+  //     setCustomerEmail(data?.customer_email as any);
+  //   });
+  // }, []);
 
-  useEffect(() => {
-    getCheckoutSession(sessionId).then((data) => {
-      setStatus(data?.status as any);
-      setCustomerEmail(data?.customer_email as any);
-    });
-  }, []);
+  // if (status === "open") {
+  //   return router.push("/");
+  // }
 
-  if (status === "open") {
-    return redirect("/");
-  }
+  // if (status === "complete") {
+  //   return (
+  //     <section id="success">
+  //       <p>
+  //         We appreciate your business! A confirmation email will be sent to{" "}
+  //         {customerEmail}. If you have any questions, please email{" "}
+  //         <a href="mailto:orders@example.com">orders@example.com</a>.
+  //       </p>
+  //     </section>
+  //   );
+  // }
 
-  if (status === "complete") {
-    return (
-      <section id="success">
-        <p>
-          We appreciate your business! A confirmation email will be sent to{" "}
-          {customerEmail}. If you have any questions, please email{" "}
-          <a href="mailto:orders@example.com">orders@example.com</a>.
-        </p>
-      </section>
-    );
-  }
-
-  return null;
+  return (
+    <section id="loading">
+      <p>Processing your payment...</p>
+    </section>
+  );
 }
