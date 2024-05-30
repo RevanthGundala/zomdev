45eec3200cf53f666b4f75e55a1ac8d3fc24bbae
RevanthGundala <93841932+RevanthGundala@users.noreply.github.com>
2024-05-30 14:17:47 -0500

    added blockeden

--- zomdev/app/actions/contract/helpers/getSuiClient.ts
+++ zomdev/app/actions/contract/helpers/getSuiClient.ts
@@ -8,12 +8,18 @@ export async function getSuiClient(): Promise<SuiClient> {
       : createSuiClient(process.env.NODE_ACCESS_KEY_TEST!);
   } catch (e) {
     console.error("Error getting SuiClient:", e);
+    if (process.env.NEXT_PUBLIC_SUI_NETWORK === "mainnet") {
+      try {
+        const apiAccessKey = process.env.NEXT_PUBLIC_BLOCK_EDEN_KEY;
+        const blockEdenUrl = ` https://api.blockeden.xyz/sui/${apiAccessKey}`;
+        return new SuiClient({ url: blockEdenUrl });
+      } catch (e) {
+        console.error("Error getting SuiClient from BlockEden:", e);
+        return new SuiClient({ url: getFullnodeUrl("mainnet") });
+      }
+    }
     return new SuiClient({
-      url: getFullnodeUrl(
-        process.env.NEXT_PUBLIC_SUI_NETWORK === "mainnet"
-          ? "mainnet"
-          : "testnet"
-      ),
+      url: getFullnodeUrl("testnet"),
     });
   }
 }
